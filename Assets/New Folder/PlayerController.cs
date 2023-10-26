e8f4c5de8b710ac4707a105c21d979245dbd4df2
sssmart24 <ssmart24@aoiths.org>
2023-10-26 12:48:19 -0400

    pop

--- Assets/New Folder/PlayerController.cs
+++ Assets/New Folder/PlayerController.cs
@@ -2,8 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerControllerScript : MonoBehaviour
-{
+public class PlayerController : MonoBehaviour
+{  
+    public float speed = 20;
+    
+    public float horizontalInput;
+    public float verticalInput; 
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,9 @@ public class PlayerControllerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.right * speed * Time.deltaTime * horizontalInput);
+        transform.Translate(Vector3.forward * speed * Time.deltaTime * verticalInput);
     }
 }
